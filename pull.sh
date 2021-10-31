f9a901abe5fa8b10f376618816384842e0596956
Lucifer <82418@163.com>
2021-10-31 23:44:33 +0800

    shell

--- pull.sh
+++ pull.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-git pull && yarn build && cp -R /root/notes/.vuepress/dist /usr/share/nginx/html/notes
+git pull && yarn build && cp -Rf /root/notes/.vuepress/dist /usr/share/nginx/html/notes
